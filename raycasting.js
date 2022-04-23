328370bb0fab2b8f95f3e8f86a776c38689a24c6
Ariya Hidayat <ariya.hidayat@gmail.com>
2022-04-23 09:38:43 -0700

    event handler

--- raycasting.js
+++ raycasting.js
@@ -1,3 +1,30 @@
 function init() {
     console.log('Initializing...');
+
+    document.addEventListener("keydown", (event) => {
+        const { key } = event;
+        switch (key) {
+            case 'a': turnLeft(); break;
+            case 'd': turnRight(); break;
+            case 'w': moveForward(); break;
+            case 's': moveBackward(); break;
+        }
+    });
+
+    let player = { x: 1.1, y: 1.7, angle: 6.1 };
+
+    function turnLeft(a = 0.1) {
+        player.angle -= a;
+    }
+    function turnRight(a = 0.1) {
+        turnLeft(-a);
+    }
+    function moveForward(step = 0.1) {
+        player.x += step * Math.cos(player.angle);
+        player.y += step * Math.sin(player.angle);
+    }
+    function moveBackward(step = 0.1) {
+        moveForward(-step);
+    }
+
 }
